a20312d3d2b68eddd39fb79475ae40c9ec699d54
eliot-akira <me@eliotakira.com>
2019-04-09 06:31:12 +0200

    Separate markdown module for use by builder and others

---
+++ markdown/index.js
@@ -0,0 +1,24 @@
+const frontmatter = require('front-matter')
+
+const md = require('markdown-it')({
+  preset: 'default',
+  html: true,
+  linkify: true
+  //highlight: renderHighLight
+})
+  .use(require('./taskList'), {
+    // Without `disabled` attribute
+    enabled: true
+  })
+  .use(require('./anchor'))
+
+module.exports = function renderMarkdown(content = '', options = {}) {
+
+  const { body, attributes } = frontmatter(content)
+
+  const html = options.markdown
+    ? options.markdown(body)
+    : md.render(body)
+
+  return { html, attributes }
+}
---
+++ markdown/index.test.js
@@ -0,0 +1,34 @@
+const renderMarkdown = require('./index')
+
+test('Markdown', (it, { isEqual }) => {
+
+  it('runs', renderMarkdown())
+
+  const source =
+`---
+title: Title
+tags: [abc, def]
+---
+
+# H1
+
+[Link](http://example.com)
+
+`
+
+  const snapshot = {
+    html: '<h1 id="h1">H1</h1>\n<p><a href="http://example.com">Link</a></p>\n',
+    attributes: {
+      title: 'Title',
+      tags: [ 'abc', 'def' ]
+    }
+  }
+
+  const result = renderMarkdown(source)
+
+  it('returns rendered html and attributes from Markdown front matter', result.html && result.attributes)
+  it('renders html correctly', result.html===snapshot.html)
+  it('parses Markdown frontmatter correctly',
+    isEqual(snapshot.attributes, result.attributes)
+  )
+})
