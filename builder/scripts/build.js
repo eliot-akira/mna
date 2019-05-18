ca1b57426ec744f54e913f7c75ef7f2aacac7e49
eliot-akira <me@eliotakira.com>
2019-05-18 21:01:45 +0200

    Ensure .log folder for pm2

--- builder/scripts/build.js
+++ builder/scripts/build.js
@@ -83,6 +83,10 @@ async function generateProductionServerPackage({ paths }) {
     pm2ConfigJs = `
 // PM2 process file
 // http://pm2.keymetrics.io/docs/usage/application-declaration/
+
+// Make sure log directory exists
+try { require('fs').mkdirSync('.log') } catch (e) { /**/ }
+
 module.exports = {
   name: require('./package.json').name,
   script: '${serverEntry}',
