c380b42044b294dee7d2768b712e888e8f62b6d7
eliot-akira <me@eliotakira.com>
2019-06-03 13:16:09 +0200

    markdown: update test for heading anchor

--- markdown/index.test.js
+++ markdown/index.test.js
@@ -17,7 +17,7 @@ tags: [abc, def]
 `
 
   const snapshot = {
-    html: '<h1 id="h1">H1</h1>\n<p><a href="http://example.com">Link</a></p>\n',
+    html: '<h1><a name="h1" class="markdown-heading-anchor" href="#"></a>H1</h1>\n<p><a href="http://example.com">Link</a></p>\n',
     attributes: {
       title: 'Title',
       tags: [ 'abc', 'def' ]
@@ -27,7 +27,7 @@ tags: [abc, def]
   const result = renderMarkdown(source)
 
   it('returns rendered html and attributes from Markdown front matter', result.html && result.attributes)
-  it('renders html correctly', result.html===snapshot.html)
+  it('renders html correctly', result.html===snapshot.html, { snapshot: snapshot.html, result: result.html })
   it('parses Markdown frontmatter correctly',
     it.is(snapshot.attributes, result.attributes)
   )
